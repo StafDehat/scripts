2a941a791f480ae0e050e572b9742801007b36a4
Andrew Howard <ahoward@phoenix.rootmypc.net>
2014-02-24 17:12:07 -0600

    Added show-grants

---
+++ mysql-show-grants.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mysql mysql --skip-column-names -e "select Host, User from user;" | while read x; do
+ HOST=`echo $x | awk '{print $1}'`
+ USER=`echo $x | awk '{print $2}'`
+ mysql --skip-column-names -e "show grants for '$USER'@'$HOST';" | sed -e 's/\\\\/\\/g' -e 's/$/;/'
+done 2>/dev/null
