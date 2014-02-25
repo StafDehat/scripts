f4c084958d042af8730f25980b3b3818cdb60b1d
Andrew Howard <ahoward@phoenix.rootmypc.net>
2014-02-25 15:50:16 -0600

    Added git-scrub to purge a given file(s) from github history

--- git-scrub.sh
+++ git-scrub.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 
-
-echo git filter-branch --force --index-filter \"git rm --cached --ignore-unmatch $1\" --prune-empty --tag-name-filter cat -- --all
+echo "git stash"
+while read LINE; do
+  echo "git filter-branch --force --index-filter \"git rm --cached --ignore-unmatch $LINE\" --prune-empty --tag-name-filter cat -- --all"
+done
+echo "git push origin master --force"
+echo "git stash apply"
