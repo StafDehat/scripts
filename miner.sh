5a7c8dbde8a7cf5ea5bba50da16ac0ea9d993da7
Andrew Howard <ahoward@phoenix.rootmypc.net>
2014-03-05 18:47:13 -0600

    Adjusted miner.sh

--- miner.sh
+++ miner.sh
@@ -8,12 +8,13 @@
 USERNAME=
 WORKER=`hostname | cut -d. -f1`
 PASSWORD=`hostname | cut -d. -f1`
+SHELLUSER=
 DOGESERVER=stratum.dogehouse.org:8081
 QUARKSERVER=mine-pool.net:3350
-MINERD=~/minerd.quark
-#MINERD=~/minerd.doge
+MINERD=`eval echo ~$SHELLUSER/minerd.quark`
+#MINERD=`eval echo ~$SHELLUSER/minerd.doge`
+PIDFILE=`eval echo ~$SHELLUSER/.crypto.pid`
 SCREENNAME=crypto
-PIDFILE=/var/run/.crypto.pid
 
 function start() {
   if [ -e $PIDFILE ]; then
@@ -30,28 +31,37 @@ function start() {
     fi
   fi
   # Doge
-  # screen -d -m -S dogecoin $MINERD -o stratum+tcp://$DOGESERVER -u $USERNAME.$WORKER -p $PASSWORD
+  # sudo -u $SHELLUSER screen -d -m -S dogecoin $MINERD -o stratum+tcp://$DOGESERVER -u $USERNAME.$WORKER -p $PASSWORD
 
   # Quark
-  screen -d -m -S $SCREENNAME $MINERD -a quark -o stratum+tcp://$QUARKSERVER -u $USERNAME.$WORKER -p $WORKER
+  sudo -u $SHELLUSER screen -d -m -S $SCREENNAME $MINERD -a quark -o stratum+tcp://$QUARKSERVER -u $USERNAME.$WORKER -p $WORKER
 
-  PID=`screen -list | grep $SCREENNAME | cut -d. -f1`
+  PID=`sudo -u $SHELLUSER screen -list | grep $SCREENNAME | cut -d. -f1`
   echo $PID > $PIDFILE
   echo "Miner process ($MINERD) started."
 }
 
 function stop() {
-  if [ ! -e $PIDFILE ]; then
-    echo "PID file does not exist.  Is the process actually running?"
-    exit 1
+  if [ -e $PIDFILE ]; then
+    PID=`cat $PIDFILE`
+    echo "Killing PID $PID"
+    kill $PID
+    sleep 5
+    if [ -e /proc/$PID ]; then
+      echo "Force-killing PID $PID"
+      kill -9 $PID
+    fi
+    rm -f $PIDFILE
   fi
-  PID=`cat $PIDFILE`
-  kill $PID
-  sleep 5
-  if [ -e /proc/$PID ]; then
-    kill -9 $PID
+  if [ `ps aux | grep -c [m]inerd` -gt 0 ]; then
+    echo "Killing all minerd processes"
+    killall $MINERD
+    sleep 5
   fi
-  rm -f $PIDFILE
+  if [ `ps aux | grep -c [m]inerd` -gt 0 ]; then
+    echo "Force-killing all minerd processes"
+    killall -9 $MINERD
+  fi
   echo "Process has been stopped, probably."
 }
 
@@ -66,15 +76,20 @@ case $1 in
     if [ -e $PIDFILE ]; then
       PID=`cat $PIDFILE`
       echo "PID file exists with PID $PID"
+      if [ `ps aux | awk '$2 ~ /^'$PID'$/ {print}' | grep -c $MINERD` -gt 0 ]; then
+        echo "And process $PID is our minerd process."
+        if [ `sudo -u $SHELLUSER screen -list | grep -c $PID.$SCREENNAME` -gt 0 ]; then
+          echo "And it's running in the right SCREEN session."
+          exit 0
+        fi
+      fi
     fi
-    if [ `screen -list | grep $SCREENNAME | wc -l` -gt 0 ]; then
-      echo "Screen is currently running"
-    fi
-    ps uax | grep minerd
+    echo "Something's not right - tear it down and start over, dude."
     exit 1
     ;;
   restart)
     stop
+    sleep 5
     start
     ;;
 esac
