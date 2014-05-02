3e8534fd2f7be3833ac7604f5ca76e331d81334d
Andrew Howard <ahoward@phoenix.rootmypc.net>
2014-05-02 11:29:41 -0500

    Added nonexist file handling

--- mysql-entropy.sh
+++ mysql-entropy.sh
@@ -4,8 +4,16 @@
 DB=ecom
 HOST=localhost
 PORT=3306
+USER=root
+PASS=Cnw49uNyVp
 FREQ=5
-MYSQL="mysql --skip-column-names --protocol=TCP -h $HOST -P $PORT"
+MYSQL="mysql --skip-column-names --protocol=TCP -h $HOST -P $PORT -u$USER -p$PASS"
+TOGGLE=/home/lab/entropy
+
+# Pause:    echo 0 > $TOGGLE
+# Run live: echo 1 > $TOGGLE
+# Exit:     echo 2 > $TOGGLE
+# Will fail to Pause status
 
 
 #
@@ -38,7 +46,9 @@ fi
 #
 # Inserts
 while true; do
-  if [ `cat entropy` -ne 0 ]; then
+  if [ `cat $TOGGLE` -eq 2 ]; then
+    exit
+  elif [ `cat $TOGGLE` -eq 1 ]; then
     if [ $( $MYSQL $DB -e "SELECT count(id) FROM orders;" ) -lt 30 ]; then
       $MYSQL $DB -e "INSERT INTO orders (count, time) VALUES ($RANDOM, now())"
     fi
@@ -49,21 +59,33 @@ done &
 #
 # Updates
 while true; do
-  if [ `cat entropy` -ne 0 ]; then
-    # Replication-unsafe query
-    $MYSQL $DB -e "UPDATE orders SET count = '$RANDOM' ORDER BY RAND() LIMIT 1;"
+  if [ -f $TOGGLE ]; then
+    if [ `cat $TOGGLE` -eq 2 ]; then
+      exit
+    elif [ `cat $TOGGLE` -eq 1 ]; then
+      # Replication-unsafe query
+      $MYSQL $DB -e "UPDATE orders SET count = '$RANDOM' ORDER BY RAND() LIMIT 1;"
+    fi
   fi
   sleep $(( $RANDOM % $FREQ ))
 done &
 while true; do
-  if [ `cat entropy` -ne 0 ]; then
-    $MYSQL $DB -e "UPDATE products SET stock = stock - 1 WHERE stock > 0 ORDER BY RAND() LIMIT 1;"
+  if [ -f $TOGGLE ]; then
+    if [ `cat $TOGGLE` -eq 2 ]; then
+      exit
+    elif [ `cat $TOGGLE` -eq 1 ]; then
+      $MYSQL $DB -e "UPDATE products SET stock = stock - 1 WHERE stock > 0 ORDER BY RAND() LIMIT 1;"
+    fi
   fi
   sleep $(( $RANDOM % $FREQ ))
 done &
 while true; do
-  if [ `cat entropy` -ne 0 ]; then
-    $MYSQL $DB -e "UPDATE products SET stock = stock + 1 WHERE stock < 30000 ORDER BY RAND() LIMIT 1;"
+  if [ -f $TOGGLE ]; then
+    if [ `cat $TOGGLE` -eq 2 ]; then
+      exit
+    elif [ `cat $TOGGLE` -eq 1 ]; then
+      $MYSQL $DB -e "UPDATE products SET stock = stock + 1 WHERE stock < 30000 ORDER BY RAND() LIMIT 1;"
+    fi
   fi
   sleep $(( $RANDOM % $FREQ ))
 done &
@@ -71,10 +93,14 @@ done &
 #
 # Deletes
 while true; do
-  if [ `cat entropy` -ne 0 ]; then
-    if [ $( $MYSQL $DB -e "SELECT count(id) FROM orders;" ) -gt 20 ]; then
-       # Replication-unsafe query
-       $MYSQL $DB -e "DELETE FROM orders WHERE count < '30000' ORDER BY RAND() LIMIT 1;"
+  if [ -f $TOGGLE ]; then
+    if [ `cat $TOGGLE` -eq 2 ]; then
+      exit
+    elif [ `cat $TOGGLE` -eq 1 ]; then
+      if [ $( $MYSQL $DB -e "SELECT count(id) FROM orders;" ) -gt 20 ]; then
+         # Replication-unsafe query
+         $MYSQL $DB -e "DELETE FROM orders WHERE count < '30000' ORDER BY RAND() LIMIT 1;"
+      fi
     fi
   fi
   sleep $(( $RANDOM % $FREQ ))
