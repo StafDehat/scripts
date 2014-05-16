4e538256b3b92004d749604bcf2d00f55a522152
Andrew Howard <ahoward@phoenix.rootmypc.net>
2014-05-16 11:46:53 -0500

    Added script to easily retrieve an authtoken

---
+++ cloud-get-auth-token.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#!/bin/bash
+
+read -p "What's the username for your cloud account? " CLOUD_USERNAME
+read -p "And now enter your API key (not token): " CLOUD_API_KEY
+IDENTITY_ENDPOINT="https://identity.api.rackspacecloud.com/v2.0"
+TOKEN=$( curl $IDENTITY_ENDPOINT/tokens \
+           -H "Content-Type: application/json" \
+           -d '{ "auth": { 
+                 "RAX-KSKEY:apiKeyCredentials": {
+                   "apiKey": "'$CLOUD_API_KEY'",
+                   "username": "'$CLOUD_USERNAME'" } } }' 2>/dev/null )
+AUTHTOKEN=$( echo "$TOKEN" | perl -pe 's/.*"token":.*?"id":"(.*?)".*$/\1/' )
+TENANTID=$( echo "$TOKEN" | perl -pe 's/.*"token":.*?"tenant":.*?"id":"(.*?)".*$/\1/' )
+
+echo
+echo "Here's your auth token:   $AUTHTOKEN"
+echo "And your DDI (Tenant ID): $TENANTID"
