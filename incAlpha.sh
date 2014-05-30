5ed636c29d11f4f1a8b325d9b38a0a1452e38367
Andrew Howard <ahoward@phoenix.rootmypc.net>
2014-05-30 17:23:12 -0500

    Added incAlpha script

---
+++ incAlpha.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+# Increment a lowercase character string.
+# a>b, z>aa, aa>ab, zz>aaa
+function incAlpha() {
+  local ALPHA="$1"
+  local LEASTSIG=$( echo "$ALPHA" | sed 's/^.*\(.\)$/\1/' )
+  local ELSE=$( echo "$ALPHA" | sed 's/^\(.*\).$/\1/' )
+  if [ -z "$ALPHA" ]; then
+    echo 'a'
+  elif [ "$LEASTSIG" != "z" ]; then
+    LEASTSIG=$( echo "$LEASTSIG" | tr 'a-z' 'b-z0' )
+    echo "$ELSE$LEASTSIG"
+  else
+    local NEWELSE=$( incAlpha $ELSE )
+    echo "${NEWELSE}a"
+  fi
+}
+
+incAlpha $1
