fc4a6e5edb94a53ab67e28a6eae80b1c306a460d
Andrew Howard <ahoward@phoenix.rootmypc.net>
2014-06-30 13:41:58 -0500

    Added "--minimal" flag to xe commands

--- cloud-snapshot-cleanup.sh
+++ cloud-snapshot-cleanup.sh
@@ -8,12 +8,12 @@ function min() {
   fi
 }
 
-SRUUID=$( xe sr-list name-label="Local storage" params=uuid | awk '/^uuid/ {print $NF}' )
-SRSIZE=$( xe sr-list uuid=$SRUUID params=physical-size | awk '/^physical-size/ {print $NF}' )
-SRUSED=$( xe sr-list uuid=$SRUUID params=physical-utilisation | awk '/^physical-utilisation/ {print $NF}' )
+SRUUID=$( xe sr-list name-label="Local storage" params=uuid --minimal )
+SRSIZE=$( xe sr-list uuid=$SRUUID params=physical-size --minimal )
+SRUSED=$( xe sr-list uuid=$SRUUID params=physical-utilisation --minimal )
 
 # Grab the complete list of snapshots on this Hyp
-SNAPSHOTS=$( xe vdi-list is-a-snapshot=true params=uuid sr-uuid=$SRUUID | awk '/^uuid/ {print $NF}' )
+SNAPSHOTS=$( xe vdi-list is-a-snapshot=true params=uuid sr-uuid=$SRUUID --minimal | tr ',' ' ' )
 
 # Strip the list down to only *old* snapshots
 HOURS=24
@@ -21,7 +21,7 @@ EXPIRE=$(( 60 * 60 * $HOURS ))
 EXPIRE=$(( 60 * 60 * $HOURS ))
 OLDSNAPS=""
 for SNAPSHOT in $SNAPSHOTS; do
-  DATE=$( xe vdi-list uuid=$SNAPSHOT params=snapshot-time | awk '{print $NF}' )
+  DATE=$( xe vdi-list uuid=$SNAPSHOT params=snapshot-time --minimal )
   DATE=$( date +%s -d "$( echo $DATE | sed 's/T/ /' )" )
   NOW=$( date +%s )
   AGE=$(( $NOW - $DATE ))
@@ -40,9 +40,9 @@ for SNAPSHOT in $OLDSNAPS; do
     echo "Unable to identify parent VHD of $SNAPSHOT - skipping this one"
   else
     DELETIONS="$DELETIONS $SNAPSHOT"
-    DiffDisk=$( xe vdi-list is-a-snapshot=false parent=$PARENT params=uuid | awk '/^uuid/ {print $NF}' )
+    DiffDisk=$( xe vdi-list is-a-snapshot=false parent=$PARENT params=uuid --minimal )
     DiffSize=$( du -b /var/run/sr-mount/$SRUUID/$DiffDisk.vhd | awk '{print $1}' )
-    ParentVirtualSize=$( xe vdi-list params=virtual-size uuid=$PARENT | awk '/^virtual-size/ {print $NF}' )
+    ParentVirtualSize=$( xe vdi-list params=virtual-size uuid=$PARENT --minimal )
     ParentRealSize=$( du -b /var/run/sr-mount/$SRUUID/$PARENT.vhd | awk '{print $1}' )
     MaxDiskIncrease=$(( $MaxDiskIncrease +
                         $( min $DiffSize \
