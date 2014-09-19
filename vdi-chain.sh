ad070de19a0c46cbc38ff45667400ea1e62b9a2a
Andrew Howard <ahoward@phoenix.rootmypc.net>
2014-09-19 11:27:18 -0500

    Added some more null checks

--- vdi-chain.sh
+++ vdi-chain.sh
@@ -77,10 +77,13 @@ while read LINE; do
                         if (sum>=x) { printf "%.2f %s\n",sum/x,hum[x];break } }}' )
   SNAPSHOT=$( xe vdi-param-get param-name=is-a-snapshot uuid=$VDIUUID )
   DEVICE=$( xe vbd-list params=device vdi-uuid=$VDIUUID --minimal )
-  if [ -z "$DEVICE" ]; then DEVICE="---"; fi
   PLUGGED=$( xe vbd-list params=vm-name-label vdi-uuid=$VDIUUID --minimal )
-  if [ -z "$PLUGGED" ]; then PLUGGED="---"; fi
   VMNAME=$( xe vdi-param-get param-name=name-label uuid=$VDIUUID )
+  if [ -z "$VMNAME" ]; then VMNAME="---"; fi
+  if [ -z "$PLUGGED" ]; then PLUGGED="---"; fi
+  if [ -z "$DEVICE" ]; then DEVICE="---"; fi
+  if [ -z "$VHDSIZE" ]; then VHDSIZE="---"; fi
+  if [ -z "$SNAPSHOT" ]; then SNAPSHOT="---"; fi
   echo "$SPACING$VMNAME:$PLUGGED:$DEVICE:$VHDSIZE:$SNAPSHOT:$VDIUUID"
 done <<<"$VHDOUTPUT"
 ) | column -s : -t
