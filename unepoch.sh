7a3d92f56ec71818ee12cb578eb241e17de913c8
Andrew Howard <ahoward@phoenix.rootmypc.net>
2014-10-01 09:41:05 -0500

    Trivial style changes

--- unepoch.sh
+++ unepoch.sh
@@ -6,6 +6,6 @@ while read LINE; do
   echo $LINE | perl -pe 's/(\d+)/localtime($1)/e'
 done
 
-# It's way better to just do this:
-# alias unepoch="perl -pe 's/(\d+)/localtime(\$1)/e'"
+# It's way better to just do this
+#alias unepoch="perl -pe 's/(\d+)/localtime(\$1)/e'"
 
