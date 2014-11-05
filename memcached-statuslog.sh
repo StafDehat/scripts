c6ed1bf137836e8d9d93c5875687bc622f4cd670
Andrew Howard <ahoward@phoenix.rootmypc.net>
2014-11-05 11:53:00 -0600

    Added some memcache logging & reporting

---
+++ memcached-statuslog.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Author: Andrew Howard
+# A race-condition-safe bash script wrapper that will ensure this script
+# runs non-concurrently with other instances of itself.
+
+LOCK_FILE=/tmp/`basename $0`.lock
+function cleanup {
+ echo "Caught exit signal - deleting trap file"
+ rm -f $LOCK_FILE
+ exit 2
+}
+trap 'cleanup' 1 2 9 15 17 19 23 EXIT
+(set -C; : > $LOCK_FILE) 2> /dev/null
+if [ $? != "0" ]; then
+ echo "Lock File exists - exiting"
+ exit 1
+fi
+
+LOGDIR=/var/log/memcached
+
+if [ $( basename $LOGDIR ) != "memcached" ]; then
+  echo "LOGDIR must be named 'memcached'." >&2
+  echo "I insist on this as a safety check, because we're going" >&2
+  echo "  to run tmpwatch on it." >&2
+  exit
+fi
+if [ ! -d $LOGDIR ]; then
+  if [ $( id -u ) -eq 0 ]; then
+    mkdir -p $LOGDIR
+  else
+    echo "$0: Log directory ($LOGDIR) does not exist" >&2
+    exit
+  fi
+fi
+
+cat << EOF >$LOGDIR/$( date +"%F-%T" )
+$( date )
+
+$( echo stats | nc localhost 11211 )
+EOF
+
+tmpwatch -m 24 $LOGDIR
+
---
+++ memcached-statusreport.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Author: Andrew Howard
+# Holy shit, indirect references
+
+# After created logs of memcache stats with memcached-statuslog.sh, you can
+#   use this script to print a report of how many operations occurred each
+#   interval.
+# Example usage:
+#   ./memcached-statusreport.sh cmd_get cmd_set curr_items
+
+if [ $# -lt 1 ]; then
+  echo "Gotta pass args"
+  exit
+fi
+
+(
+  for VAR in $@; do
+    declare NOW_$VAR=0
+  done
+  echo "Time $@"
+  for x in /var/log/memcached/*; do
+    TIMESTAMP=$( basename $x )
+    echo -n "$TIMESTAMP "
+
+    for VAR in $@; do
+      declare THEN_$VAR=$(eval "echo \$NOW_$VAR")
+      declare NOW_$VAR="$( grep $VAR $x | sed 's/[^0-9]*\([0-9]*\).*/\1/' )"
+      declare DIFF_$VAR=$(( $(eval "echo \$NOW_$VAR") - $(eval "echo \$THEN_$VAR") ))
+      ECHO=$( eval "echo \$DIFF_$VAR" )
+      echo -n "$ECHO "
+    done
+
+    echo
+  done
+) | column -t
+
+
