86f85776ffc74134ef4d7247bd6f5ce6bd98e407
Andrew Howard <ahoward@phoenix.rootmypc.net>
2014-12-12 11:54:43 -0600

    Added logging and updated non-concurrency check

--- perms-backup.sh
+++ perms-backup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-
 # Author: Andrew Howard
+# A race-condition-safe bash script wrapper that will ensure this script
+# runs non-concurrently with other instances of itself.
+
+logger "Beginning run of script: $0"
 
 LOCK_FILE=/tmp/`basename $0`.lock
 function cleanup {
+ logger "Exiting script: $0"
  echo "Caught exit signal - deleting trap file"
  rm -f $LOCK_FILE
  exit 2
@@ -12,6 +16,7 @@ trap 'cleanup' 1 2 9 15 17 19 23 EXIT
 (set -C; : > $LOCK_FILE) 2> /dev/null
 if [ $? != "0" ]; then
  echo "Lock File exists - exiting"
+ logger "Lock File exists - exiting script: $0"
  exit 1
 fi
 
