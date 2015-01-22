d65b657ffd1c42a9c81ef0eb3a20db68ac41b12b
Andrew Howard <ahoward@phoenix.rootmypc.net>
2015-01-22 14:24:25 -0600

    Added parameter substitution

--- bash-variables.sh
+++ bash-variables.sh
@@ -1,14 +1,19 @@
 # Author: Andrew Howard
 
-$$	Process ID (PID) of the script itself.
-$#	Number of arguments passed to this process
-$0	Full path to currently-running executable
-$1	First argument passed to this process
-$2	Second argument passed to this process, etc for $3...
-$_	Final argument passed to previous command
-$*	An eval'd string of all my arguments.  You probably wanted $@.
-$@	A quoted string, not eval'd, of all my arguments
-$-	Flags passed to this script.  In "echo -n blah", it would be "n".
-$!	PID of last job running in background.
-$?	Exit status of most recently-completed child process
-${!x}	Indirect reference.  y=2; x=y; echo ${!x};
+$$		Process ID (PID) of myself.
+$#		Number of arguments passed to me.
+$?		Exit status of most recently-completed child process.
+$0		Currently-running executable (not absolute path).
+$1		First argument passed to this process.  Likewise for $2, $3...
+$_		Final argument passed to previous command
+$*		An eval'd string of all my arguments.  You probably wanted $@.
+$@		A quoted string, not eval'd, of all my arguments
+$-		Flags passed to this script.  In "echo -n blah", it would be "n".
+$!		PID of last job running in background.
+${!x}		Indirect reference.  y=2; x=y; echo ${!x};  #(prints 2)
+
+# Parameter substitution
+${x/A/B}	Once.  s/A/B/ <<<$x
+${x//A/B}	Global.  s/A/B/g <<<$x
+${x/#A/B}	Prefix.  s/^A/B/ <<<$x
+${x/%A/B}	Suffix.  s/A$/B/ <<<$x
