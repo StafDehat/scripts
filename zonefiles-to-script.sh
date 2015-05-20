6c81e5f3736029542b488594d64a661394bc9863
Andrew Howard <ahoward@phoenix.rootmypc.net>
2015-05-20 12:49:23 -0500

    Added my own reporting

--- zonefiles-to-script.sh
+++ zonefiles-to-script.sh
@@ -33,185 +33,191 @@ function usage() {
 function addzones() {
   #
   # Attempt to create the zones on ACCT
+  NUMZONES=0
   for ZONE in $ZONES; do
     echo "add_default_zone $ZONE $ACCT"
+    NUMZONES=$(( $NUMZONES + 1 ))
   done
   echo
   #
   # Record in appstats that this was executed.
-  curl -s https://appstats.rackspace.com/appstats/event/ \
-       -X POST \
-       -H "Content-Type: application/json" \
-       -d '{ "username": "andrew.howard",
-             "status": "SUCCESS",
-             "bizunit": "Enterprise",
-             "OS": "Linux",
-             "functionid": "Part1-Zones",
-             "source": "https://github.com/StafDehat/scripts/blob/master/zonefiles-to-script.sh",
-             "version": "1.0",
-             "appid": "zonefiles-to-script.sh",
-             "device": "N/A",
-             "ip": "",
-             "datey": "'$(date +%Y)'",
-             "datem": "'$(date +%-m)'",
-             "dated": "'$(date +%-d)'",
-             "dateh": "'$(date +%-H)'",
-             "datemin": "'$(date +%-M)'",
-             "dates": "'$(date +%-S)'"
-             }' &>/dev/null
+  ( curl -s https://appstats.rackspace.com/appstats/event/ \
+         -X POST \
+         -H "Content-Type: application/json" \
+         -d '{ "username": "andrew.howard",
+               "status": "SUCCESS",
+               "bizunit": "Enterprise",
+               "OS": "Linux",
+               "functionid": "Part1-Zones",
+               "source": "https://github.com/StafDehat/scripts/blob/master/zonefiles-to-script.sh",
+               "version": "1.0",
+               "appid": "zonefiles-to-script.sh",
+               "device": "N/A",
+               "ip": "",
+               "datey": "'$(date +%Y)'",
+               "datem": "'$(date +%-m)'",
+               "dated": "'$(date +%-d)'",
+               "dateh": "'$(date +%-H)'",
+               "datemin": "'$(date +%-M)'",
+               "dates": "'$(date +%-S)'"
+               }' &) &>/dev/null
+  #
+  # Report usage stats to author's tracking tool
+  (curl -k "https://stats.rootmypc.net/dnsstats.php?zones=$NUMZONES&records=0" &) &>/dev/null
 }
 
 #
 # This gets run if argument $1 was '2'
 function addrecords() {
+  NUMRECORDS=0
   for ZONE in $ZONES; do
     #
     # A records
-    grep -iP '^\s*[^\s]+\s+(\d+[^\s]*\s+)?(IN\s+)?A\s+' $ZONE |
-      sed -e 's/\s*\(;.*\)\?$//' -e "s/@/$ZONE./" |
-      sed '/^\s*$/d' | # Delete empty lines
-      while read LINE; do
-        RECORD=$( echo "$LINE" | awk '{print $1}' | sed 's/\s*$//' )
-        if grep -qP '\.$' <<<$RECORD; then
-          RECORD=$( echo "$RECORD" | sed 's/\.$//' )
-        else
-          RECORD="$RECORD.$ZONE"
-        fi
-        TARGET=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $NF}' )
-        echo "add_address_record $ZONE $RECORD $TARGET"
-      done
+    while read LINE; do
+      RECORD=$( echo "$LINE" | awk '{print $1}' | sed 's/\s*$//' )
+      if grep -qP '\.$' <<<$RECORD; then
+        RECORD=$( echo "$RECORD" | sed 's/\.$//' )
+      else
+        RECORD="$RECORD.$ZONE"
+      fi
+      TARGET=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $NF}' )
+      echo "add_address_record $ZONE $RECORD $TARGET"
+      NUMRECORDS=$(( $NUMRECORDS + 1 ))
+    done < <( grep -iP '^\s*[^\s]+\s+(\d+[^\s]*\s+)?(IN\s+)?A\s+' $ZONE |
+                sed -e 's/\s*\(;.*\)\?$//' -e "s/@/$ZONE./" |
+                sed '/^\s*$/d' )
     
     #
     # AAAA records
-    grep -iP '^\s*[^\s]+\s+(\d+[^\s]*\s+)?(IN\s+)?AAAA\s+' $ZONE |
-      sed -e 's/\s*\(;.*\)\?$//' -e "s/@/$ZONE./" |
-      sed '/^\s*$/d' | # Delete empty lines
-      while read LINE; do
-        RECORD=$( echo "$LINE" | awk '{print $1}' | sed 's/\s*$//' )
-        if grep -qP '\.$' <<<"$RECORD"; then
-          RECORD=$( echo "$RECORD" | sed 's/\.$//' )
-        else
-          RECORD="$RECORD.$ZONE"
-        fi
-        TARGET=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $NF}' )
-        echo "add_aaaa_record $ZONE $RECORD $TARGET"
-      done
+    while read LINE; do
+      RECORD=$( echo "$LINE" | awk '{print $1}' | sed 's/\s*$//' )
+      if grep -qP '\.$' <<<"$RECORD"; then
+        RECORD=$( echo "$RECORD" | sed 's/\.$//' )
+      else
+        RECORD="$RECORD.$ZONE"
+      fi
+      TARGET=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $NF}' )
+      echo "add_aaaa_record $ZONE $RECORD $TARGET"
+      NUMRECORDS=$(( $NUMRECORDS + 1 ))
+    done < <( grep -iP '^\s*[^\s]+\s+(\d+[^\s]*\s+)?(IN\s+)?AAAA\s+' $ZONE |
+                sed -e 's/\s*\(;.*\)\?$//' -e "s/@/$ZONE./" |
+                sed '/^\s*$/d' )
     
     #
     # CNAME records
-    grep -iP '^\s*[^\s]+\s+(\d+[^\s]*\s+)?(IN\s+)?CNAME\s+' $ZONE |
-      sed -e 's/\s*\(;.*\)\?$//' -e "s/@/$ZONE./" |
-      sed '/^\s*$/d' | # Delete empty lines
-      while read LINE; do
-        RECORD=$( echo "$LINE" | awk '{print $1}' | sed 's/\s*$//' )
-        if grep -qP '\.$' <<<"$RECORD"; then
-          RECORD=$( echo "$RECORD" | sed 's/\.$//' )
-        else
-          RECORD="$RECORD.$ZONE"
-        fi
-        TARGET=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $NF}' )
-        if grep -qP '\.$' <<<"$TARGET"; then
-          TARGET=$( echo "$TARGET" | sed 's/\.$//' )
-        else
-          TARGET="$TARGET.$ZONE"
-        fi
-        echo "add_cname_record $ZONE $RECORD $TARGET"
-      done
+    while read LINE; do
+      RECORD=$( echo "$LINE" | awk '{print $1}' | sed 's/\s*$//' )
+      if grep -qP '\.$' <<<"$RECORD"; then
+        RECORD=$( echo "$RECORD" | sed 's/\.$//' )
+      else
+        RECORD="$RECORD.$ZONE"
+      fi
+      TARGET=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $NF}' )
+      if grep -qP '\.$' <<<"$TARGET"; then
+        TARGET=$( echo "$TARGET" | sed 's/\.$//' )
+      else
+        TARGET="$TARGET.$ZONE"
+      fi
+      echo "add_cname_record $ZONE $RECORD $TARGET"
+      NUMRECORDS=$(( $NUMRECORDS + 1 ))
+    done < <( grep -iP '^\s*[^\s]+\s+(\d+[^\s]*\s+)?(IN\s+)?CNAME\s+' $ZONE |
+                sed -e 's/\s*\(;.*\)\?$//' -e "s/@/$ZONE./" |
+                sed '/^\s*$/d' )
     
     #
     # MX records
-    grep -iP '^\s*[^\s]+\s+(\d+[^\s]*\s+)?(IN\s+)?MX\s+\d+\s+' $ZONE |
-      sed -e 's/\s*\(;.*\)\?$//' -e "s/@/$ZONE./" |
-      sed '/^\s*$/d' | # Delete empty lines
-      while read LINE; do
-        RECORD=$( echo "$LINE" | awk '{print $1}' | sed 's/\s*$//' )
-        if grep -qP '\.$' <<<"$RECORD"; then
-          RECORD=$( echo "$RECORD" | sed 's/\.$//' )
-        else
-          RECORD="$RECORD.$ZONE"
-        fi
-        TARGET=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $NF}' )
-        if grep -qP '\.$' <<<"$TARGET"; then
-          TARGET=$( echo "$TARGET" | sed 's/\.$//' )
-        else
-          TARGET="$TARGET.$ZONE"
-        fi
-        PRIORITY=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $(NF-1)}' )
-        echo "add_mx_record $ZONE $RECORD $PRIORITY $TARGET"
-      done
+    while read LINE; do
+      RECORD=$( echo "$LINE" | awk '{print $1}' | sed 's/\s*$//' )
+      if grep -qP '\.$' <<<"$RECORD"; then
+        RECORD=$( echo "$RECORD" | sed 's/\.$//' )
+      else
+        RECORD="$RECORD.$ZONE"
+      fi
+      TARGET=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $NF}' )
+      if grep -qP '\.$' <<<"$TARGET"; then
+        TARGET=$( echo "$TARGET" | sed 's/\.$//' )
+      else
+        TARGET="$TARGET.$ZONE"
+      fi
+      PRIORITY=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $(NF-1)}' )
+      echo "add_mx_record $ZONE $RECORD $PRIORITY $TARGET"
+      NUMRECORDS=$(( $NUMRECORDS + 1 ))
+    done < <( grep -iP '^\s*[^\s]+\s+(\d+[^\s]*\s+)?(IN\s+)?MX\s+\d+\s+' $ZONE |
+                  sed -e 's/\s*\(;.*\)\?$//' -e "s/@/$ZONE./" |
+                  sed '/^\s*$/d' )
     
     #
     # TXT/SPF records
-    grep -iP '^\s*[^\s]+\s+(\d+[^\s]*\s+)?(IN\s+)?(TXT|SPF)\s+' $ZONE |
-      sed "s/^\(\([^\"';]*|\"[^\"]*\"\|'[^']*'\)*\);.*$/\1/" | # Scrub trailing comments
-      sed "s/@/$ZONE./" | # Sub out @ for the zone name
-      sed '/^\s*$/d' | # Delete empty lines
-      while read LINE; do
-        RECORD=$( echo "$LINE" | awk '{print $1}' )
-        if grep -qP '\.$' <<<"$RECORD"; then
-          RECORD=$( echo "$RECORD" | sed 's/\.$//' )
-        else
-          RECORD="$RECORD.$ZONE"
-        fi
-        TARGET=$( echo "$LINE" | sed 's/.*\s\(TXT\|SPF\)\s\s*\(.*\)\s*$/\2/i' )
-        echo "add_txt_record $ZONE $RECORD $TARGET"
-      done
+    while read LINE; do
+      RECORD=$( echo "$LINE" | awk '{print $1}' )
+      if grep -qP '\.$' <<<"$RECORD"; then
+        RECORD=$( echo "$RECORD" | sed 's/\.$//' )
+      else
+        RECORD="$RECORD.$ZONE"
+      fi
+      TARGET=$( echo "$LINE" | sed 's/.*\s\(TXT\|SPF\)\s\s*\(.*\)\s*$/\2/i' )
+      echo "add_txt_record $ZONE $RECORD $TARGET"
+      NUMRECORDS=$(( $NUMRECORDS + 1 ))
+    done < <( grep -iP '^\s*[^\s]+\s+(\d+[^\s]*\s+)?(IN\s+)?(TXT|SPF)\s+' $ZONE |
+                  sed "s/^\(\([^\"';]*|\"[^\"]*\"\|'[^']*'\)*\);.*$/\1/" | # Scrub trailing comments
+                  sed "s/@/$ZONE./" | # Sub out @ for the zone name
+                  sed '/^\s*$/d' )
   
     #
     # SRV records
-    grep -iP '^\s*[^\s]+\s+(\d+[^\s]*\s+)?(IN\s+)?SRV\s+\d+\s+\d+\s+\d+\s+' $ZONE |
-      sed -e 's/\s*\(;.*\)\?$//' -e "s/@/$ZONE./" |
-      sed '/^\s*$/d' | # Delete empty lines
-      while read LINE; do
-        RECORD=$( echo "$LINE" | awk '{print $1}' | cut -d. -f3- )
-        if [ -z "$RECORD" ]; then
-          RECORD="$ZONE"
-        elif grep -qP '\.$' <<<"$RECORD"; then
-          RECORD=$( echo "$RECORD" | sed 's/\.$//' )
-        else
-          RECORD="$RECORD.$ZONE"
-        fi
-        SERVICE=$( echo "$LINE" | awk '{print $1}' | cut -d. -f1 )
-        PROTOCOL=$( echo "$LINE" | awk '{print $1}' | cut -d. -f2 )
-        TARGET=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $NF}' )
-        if grep -qP '\.$' <<<"$TARGET"; then
-          TARGET=$( echo "$TARGET" | sed 's/\.$//' )
-        else
-          TARGET="$TARGET.$ZONE"
-        fi
-        PORT=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $(NF-1)}' )
-        WEIGHT=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $(NF-2)}' )
-        PRIORITY=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $(NF-3)}' )
-        echo "add_srv_record $ZONE $RECORD $TARGET $SERVICE $PROTOCOL $PORT $WEIGHT $PRIORITY"
-      done
+    while read LINE; do
+      RECORD=$( echo "$LINE" | awk '{print $1}' | cut -d. -f3- )
+      if [ -z "$RECORD" ]; then
+        RECORD="$ZONE"
+      elif grep -qP '\.$' <<<"$RECORD"; then
+        RECORD=$( echo "$RECORD" | sed 's/\.$//' )
+      else
+        RECORD="$RECORD.$ZONE"
+      fi
+      SERVICE=$( echo "$LINE" | awk '{print $1}' | cut -d. -f1 )
+      PROTOCOL=$( echo "$LINE" | awk '{print $1}' | cut -d. -f2 )
+      TARGET=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $NF}' )
+      if grep -qP '\.$' <<<"$TARGET"; then
+        TARGET=$( echo "$TARGET" | sed 's/\.$//' )
+      else
+        TARGET="$TARGET.$ZONE"
+      fi
+      PORT=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $(NF-1)}' )
+      WEIGHT=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $(NF-2)}' )
+      PRIORITY=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $(NF-3)}' )
+      echo "add_srv_record $ZONE $RECORD $TARGET $SERVICE $PROTOCOL $PORT $WEIGHT $PRIORITY"
+      NUMRECORDS=$(( $NUMRECORDS + 1 ))
+    done < <( grep -iP '^\s*[^\s]+\s+(\d+[^\s]*\s+)?(IN\s+)?SRV\s+\d+\s+\d+\s+\d+\s+' $ZONE |
+                  sed -e 's/\s*\(;.*\)\?$//' -e "s/@/$ZONE./" |
+                  sed '/^\s*$/d' )
   
     #
     # PTR records
-    grep -iP '^\s*[^\s]+\s+(\d+[^\s]*\s+)?(IN\s+)?PTR\s+' $ZONE |
-      sed -e 's/\s*\(;.*\)\?$//' |
-      sed '/^\s*$/d' | # Delete empty lines
-      sort -n |
-      while read LINE; do
-        RECORD=$( echo "$LINE" | awk '{print $1}' )
-        if [ -z "$RECORD" ]; then
-          continue  # Totally not okay
-        elif grep -qP '\.$' <<<"$RECORD"; then
-          # ie: 155.0.16.10.in-addr.arpa.
-          RECORD=$( echo "$RECORD" | sed 's/\.$//' )
-        else
-          # ie: 155
-          RECORD="$RECORD.$ZONE"
-        fi
-        RECORD=$( echo "$RECORD" | sed 's/\.in-addr\.arpa.*//' |
-                    tr '.' '\n' | tac | tr '\n' '.' | sed 's/\.\s*$//' )
-        TARGET=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $NF}' )
-        if grep -qP '\.$' <<<"$TARGET"; then
-          TARGET=$( echo "$TARGET" | sed 's/\.$//' )
-        else
-          TARGET="$TARGET.$ZONE"
-        fi
-        echo "add_ptr_record $ZONE $RECORD $TARGET"
-      done
+    while read LINE; do
+      RECORD=$( echo "$LINE" | awk '{print $1}' )
+      if [ -z "$RECORD" ]; then
+        continue  # Totally not okay
+      elif grep -qP '\.$' <<<"$RECORD"; then
+        # ie: 155.0.16.10.in-addr.arpa.
+        RECORD=$( echo "$RECORD" | sed 's/\.$//' )
+      else
+        # ie: 155
+        RECORD="$RECORD.$ZONE"
+      fi
+      RECORD=$( echo "$RECORD" | sed 's/\.in-addr\.arpa.*//' |
+                  tr '.' '\n' | tac | tr '\n' '.' | sed 's/\.\s*$//' )
+      TARGET=$( echo "$LINE" | sed 's/\s*$//' | awk '{print $NF}' )
+      if grep -qP '\.$' <<<"$TARGET"; then
+        TARGET=$( echo "$TARGET" | sed 's/\.$//' )
+      else
+        TARGET="$TARGET.$ZONE"
+      fi
+      echo "add_ptr_record $ZONE $RECORD $TARGET"
+      NUMRECORDS=$(( $NUMRECORDS + 1 ))
+    done < <( grep -iP '^\s*[^\s]+\s+(\d+[^\s]*\s+)?(IN\s+)?PTR\s+' $ZONE |
+                sed -e 's/\s*\(;.*\)\?$//' |
+                sed '/^\s*$/d' | # Delete empty lines
+                sort -n )
   
     #
     # Print a newline
@@ -221,32 +227,36 @@ function addrecords() {
 
   #
   # Record in appstats that this was executed.
-  curl -s https://appstats.rackspace.com/appstats/event/ \
-       -X POST \
-       -H "Content-Type: application/json" \
-       -d '{ "username": "andrew.howard",
-             "status": "SUCCESS",
-             "bizunit": "Enterprise",
-             "OS": "Linux",
-             "functionid": "Part2-Records",
-             "source": "https://github.com/StafDehat/scripts/blob/master/zonefiles-to-script.sh",
-             "version": "1.0",
-             "appid": "zonefiles-to-script.sh",
-             "device": "N/A",
-             "ip": "",
-             "datey": "'$(date +%Y)'",
-             "datem": "'$(date +%-m)'",
-             "dated": "'$(date +%-d)'",
-             "dateh": "'$(date +%-H)'",
-             "datemin": "'$(date +%-M)'",
-             "dates": "'$(date +%-S)'"
-             }' &>/dev/null
+  ( curl -s https://appstats.rackspace.com/appstats/event/ \
+         -X POST \
+         -H "Content-Type: application/json" \
+         -d '{ "username": "andrew.howard",
+               "status": "SUCCESS",
+               "bizunit": "Enterprise",
+               "OS": "Linux",
+               "functionid": "Part2-Records",
+               "source": "https://github.com/StafDehat/scripts/blob/master/zonefiles-to-script.sh",
+               "version": "1.0",
+               "appid": "zonefiles-to-script.sh",
+               "device": "N/A",
+               "ip": "",
+               "datey": "'$(date +%Y)'",
+               "datem": "'$(date +%-m)'",
+               "dated": "'$(date +%-d)'",
+               "dateh": "'$(date +%-H)'",
+               "datemin": "'$(date +%-M)'",
+               "dates": "'$(date +%-S)'"
+               }' &) &>/dev/null
+  #
+  # Report usage stats to author's tracking tool
+  (curl -k "https://stats.rootmypc.net/dnsstats.php?zones=0&records=$NUMRECORDS" &) &>/dev/null
 }
 
 
 
 cd $ZONEDIR
-ZONES=*
+# Do at least a tiny bit of verification to see if these files are, in fact, DNS zones
+ZONES=$( grep -li soa * )
 
 if [ -z "$1" ]; then
   usage && exit
