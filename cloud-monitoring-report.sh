0d5d85f253231886a9cc76f60524f6158be88606
Andrew Howard <ahoward@phoenix.rootmypc.net>
2015-05-26 13:24:11 -0500

    Added cloud-monitoring-report.sh starter

---
+++ cloud-monitoring-report.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Author: Andrew Howard
+
+USERNAME=
+APIKEY=
+
+IDENTITY_ENDPOINT="https://identity.api.rackspacecloud.com/v2.0"
+MONITOR_ENDPOINT="https://monitoring.api.rackspacecloud.com/v1.0"
+
+#
+# Record authtoken and DDI
+DATA=$(curl -s $IDENTITY_ENDPOINT/tokens \
+            -H "Content-Type: application/json" \
+            -d '{ "auth": { 
+                  "RAX-KSKEY:apiKeyCredentials": {
+                    "apiKey": "'$APIKEY'",
+                    "username": "'$USERNAME'" } } }' \
+            2>/dev/null )
+DATA=$( echo "$DATA" | 
+          tr '}{,' '\n' | 
+          sed -n '/token/,/serviceCatalog/p' )
+AUTHTOKEN=$( echo "$DATA" |
+               sed '/tenant/,/^\s*$/d' |
+               grep '"id":' |
+               cut -d\" -f4 )
+TENANTID=$( echo "$DATA" |
+              sed -n '/tenant/,/^\s*$/p' |
+              grep '"id":' |
+              cut -d\" -f4 )
+unset USERNAME
+unset APIKEY
+
+#
+# Pull a list of entities
+ENTITIES=$( curl $MONITOR_ENDPOINT/$TENANTID/entities \
+                 -X GET \
+                 -H "Content-Type: application/json" \
+                 -H "X-Auth-Token: $AUTHTOKEN" \
+                 2>/dev/null )
+ENTITYIDS=$( echo "$ENTITIES" |
+               grep '"id":' | 
+               cut -d\" -f4 )
+
+#
+# For each entity, pull a list of monitors
+for ENTITYID in $ENTITYIDS; do
+  CHECKS=$( curl $MONITOR_ENDPOINT/$TENANTID/entities/$ENTITYID/checks \
+                 -X GET \
+                 -H "Content-Type: application/json" \
+                 -H "X-Auth-Token: $AUTHTOKEN" \
+                 2>/dev/null )
+  CHECKIDS=$( echo "$CHECKS" |
+                grep '"id":' | 
+               cut -d\" -f4 )
+  
+done
+
