caa9aaee251ebf651b917a41ed9302126ab3b54d
Andrew Howard <ahoward@phoenix.rootmypc.net>
2015-06-24 15:15:01 -0500

    Rearranged some stuff, deleted abandoned projects

--- allcron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Author: Andrew Howard
-# Work in progress.  Goal is to print all cronjobs, sorted by times they fire.
-
-cat \
-  <(cat /var/spool/cron/* | grep -vE '(^\s*$|^\#|^SHELL|^PATH|^MAILTO|^HOME)') \
-  <(cat /etc/cron.d/* | grep -vE '(^\s*$|^\#|^SHELL|^PATH|^MAILTO|^HOME)')
--- colonize.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Author: Andrew Howard
-
-# Intention of this script is to place other scripts in /etc/profile.d/ and such
-
