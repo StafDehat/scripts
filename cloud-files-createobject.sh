8e1d00ea0f710536392be2d473d8d13cbaf9567d
Andrew Howard <andrew.howard@rackspace.com>
2015-08-04 14:08:17 -0500

    Added non-concurrency wrapper

--- cloud-files-createobject.sh
+++ cloud-files-createobject.sh
@@ -32,6 +32,24 @@
 # ahoward@phoenix[~]$ 
 
 
+# Non-concurrency wrapper
+# Since this uses hard-coded named pipes, it can't run concurrently with itself
+logger "$0: Starting execution"
+
+LOCK_FILE=/tmp/`basename $0`.lock
+function cleanup {
+ logger "$0: Caught exit signal - deleting trap file"
+ rm -f $LOCK_FILE
+ exit 2
+}
+trap 'cleanup' 1 2 9 15 17 19 23 EXIT
+(set -C; : > $LOCK_FILE) 2> /dev/null
+if [ $? != "0" ]; then
+ logger "$0: Lock File exists - exiting"
+ exit 1
+fi
+
+
 PREREQS="curl grep sed cut tr echo"
 PREREQFLAG=0
 for PREREQ in $PREREQS; do
