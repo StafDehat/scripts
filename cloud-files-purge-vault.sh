1e9cff435867327cf4e2bcc5cdc68c43ed4e29d6
Andrew Howard <andrew.howard@rackspace.com>
2015-08-24 17:02:03 -0500

    Added debug & snet

--- cloud-files-purge-vault.sh
+++ cloud-files-purge-vault.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-FILES_ENDPOINT="https://storage101.lon3.clouddrive.com/v1"
-#FILES_ENDPOINT="https://storage101.syd2.clouddrive.com/v1"
-#FILES_ENDPOINT="https://storage101.dfw1.clouddrive.com/v1"
-#FILES_ENDPOINT="https://storage101.iad3.clouddrive.com/v1"
-#FILES_ENDPOINT="https://storage101.hkg1.clouddrive.com/v1"
-#FILES_ENDPOINT="https://storage101.ord1.clouddrive.com/v1"
+FILES_ENDPOINT="https://snet-storage101.lon3.clouddrive.com/v1"
+#FILES_ENDPOINT="https://snet-storage101.syd2.clouddrive.com/v1"
+#FILES_ENDPOINT="https://snet-storage101.dfw1.clouddrive.com/v1"
+#FILES_ENDPOINT="https://snet-storage101.iad3.clouddrive.com/v1"
+#FILES_ENDPOINT="https://snet-storage101.hkg1.clouddrive.com/v1"
+#FILES_ENDPOINT="https://snet-storage101.ord1.clouddrive.com/v1"
 VAULTNAME=""
 AUTHTOKEN=""
 
@@ -27,11 +27,13 @@ while read CONTAINER; do
   # Delete all objects in the container
   while read OBJECT; do
     # Delete one object
+    echo "Deleting object $OBJECT"
     curl "$FILES_ENDPOINT/$VAULTNAME/$CONTAINER/$OBJECT" \
          -X DELETE \
          -H "X-Auth-Token: $AUTHTOKEN"
   done <<<"$OBJECTS"
   # Delete the container
+  echo "Deleting container $CONTAINER"
   curl "$FILES_ENDPOINT/$VAULTNAME/$CONTAINER" \
        -X DELETE \
        -H "X-Auth-Token: $AUTHTOKEN"
