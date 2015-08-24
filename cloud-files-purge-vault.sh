a5402330b654227bac4e08ad41126d17945a049c
Andrew Howard <andrew.howard@rackspace.com>
2015-08-24 17:49:40 -0500

    Added warning and /dev/null'd curl stats

--- cloud-files-purge-vault.sh
+++ cloud-files-purge-vault.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "This is a very dangerous script and you probably don't want to run it."
+echo "As such, there's a hard-coded exit at the top."
+echo "If you want to get fired after all, go ahead and delete the exit."
+exit 0
+
 FILES_ENDPOINT="https://snet-storage101.lon3.clouddrive.com/v1"
 #FILES_ENDPOINT="https://snet-storage101.syd2.clouddrive.com/v1"
 #FILES_ENDPOINT="https://snet-storage101.dfw1.clouddrive.com/v1"
@@ -12,14 +17,14 @@ AUTHTOKEN=""
 # Get a list of all containers on this account/region
 CONTAINERS="$( curl $FILES_ENDPOINT/$VAULTNAME?format=json \
                     -X GET \
-                    -H "X-Auth-Token: $AUTHTOKEN" |
+                    -H "X-Auth-Token: $AUTHTOKEN" 2>/dev/null |
                 python -m json.tool | 
                 sed -n '/^\s*"name": /s/^\s*"name": "\(.*\)"\s*$/\1/p' )"
 while read CONTAINER; do
   # Get a list of all objects in the container
   OBJECTS="$( curl $FILES_ENDPOINT/$VAULTNAME/$CONTAINER?format=json \
                    -X GET \
-                   -H "X-Auth-Token: $AUTHTOKEN" | 
+                   -H "X-Auth-Token: $AUTHTOKEN" 2>/dev/null | 
                 python -m json.tool | 
                 sed -n '/^\s*"name": /s/^\s*"name": "\(.*\)"\s*$/\1/p' |
                 sort -nr )"
