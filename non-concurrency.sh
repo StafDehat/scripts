949f438dbbae7194e7efe0eab8652cc179154e0b
Andrew Howard <andrew.howard@rackspace.com>
2016-05-11 10:28:24 -0500

    Add PID logging

--- non-concurrency.sh
+++ non-concurrency.sh
@@ -3,18 +3,18 @@
 # A race-condition-safe bash script wrapper that will ensure this script
 # runs non-concurrently with other instances of itself.
 
-logger "$0: Starting execution"
+logger "$0 ($$): Starting execution"
 
 LOCK_FILE=/tmp/`basename $0`.lock
 function cleanup {
- logger "$0: Caught exit signal - deleting trap file"
+ logger "$0 ($$): Caught exit signal - deleting trap file"
  rm -f $LOCK_FILE
  exit 2
 }
 trap 'cleanup' 1 2 9 15 17 19 23 EXIT
 (set -C; : > $LOCK_FILE) 2> /dev/null
 if [ $? != "0" ]; then
- logger "$0: Lock File exists - exiting"
+ logger "$0 ($$): Lock File exists - exiting"
  exit 1
 fi
  
