f04c169305170f7bc66209c6243ba97108a3d4ac
Andrew Howard <andrew.howard@rackspace.com>
2016-05-11 11:04:50 -0500

    Whoops, fix non-concurrency wrapper

--- non-concurrency.sh
+++ non-concurrency.sh
@@ -4,18 +4,19 @@
 # runs non-concurrently with other instances of itself.
 
 logger "$0 ($$): Starting execution"
-
 LOCK_FILE=/tmp/`basename $0`.lock
 function cleanup {
  logger "$0 ($$): Caught exit signal - deleting trap file"
  rm -f $LOCK_FILE
  exit 2
 }
-trap 'cleanup' 1 2 9 15 17 19 23 EXIT
-(set -C; : > $LOCK_FILE) 2> /dev/null
-if [ $? != "0" ]; then
+logger "$0 ($$): Using lockfile ${LOCK_FILE}"
+(set -C; echo "$$" > "${LOCK_FILE}") 2>/dev/null
+if [ $? -ne 0 ]; then
  logger "$0 ($$): Lock File exists - exiting"
  exit 1
+else
+  trap 'cleanup' 1 2 9 15 17 19 23 EXIT
 fi
  
 ######################################
