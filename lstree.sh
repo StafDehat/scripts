4dddc2f49f952f2d71707acbf47820a9231e29a5
Andrew Howard <ahoward@galapagos.rootmypc.net>
2017-02-28 13:23:22 -0600

    Add error condition

--- lstree.sh
+++ lstree.sh
@@ -3,15 +3,17 @@
 # Author: Andrew Howard
 
 function dirlist() {
-  if ! grep -qP '^\s*/' <<<"$1"; then
-    echo "Absolute paths only, please"
-    exit 1
-  elif [ "$1" == "/" ]; then
+  if [ "$1" == "/" ]; then
     echo -n "/"
   else
     echo -n "$( dirlist $( dirname "$1" ) ) $1"
   fi
 }
 
+
+if ! grep -qP '^\s*/' <<<"$1"; then
+  echo "Absolute paths only, please"
+  exit 1
+fi
 ls -ld $( dirlist "$1" )
 
