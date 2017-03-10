859d84b7eb192a6919425ee45785706f6a50cd16
Andrew Howard <ahoward@galapagos.rootmypc.net>
2017-03-10 10:09:51 -0600

    Crunch it

--- lstree.sh
+++ lstree.sh
@@ -3,17 +3,8 @@
 # Author: Andrew Howard
 
 function dirlist() {
-  if [ "$1" == "/" ]; then
-    echo -n "/"
-  else
-    echo -n "$( dirlist $( dirname "$1" ) ) $1"
-  fi
+  [[ "$1" == "/" ]] && echo "/" ||
+    echo "$( dirlist $( dirname "$1" ) ) $1"
 }
-
-
-if ! grep -qP '^\s*/' <<<"$1"; then
-  echo "Absolute paths only, please"
-  exit 1
-fi
 ls -ld $( dirlist "$1" )
 
