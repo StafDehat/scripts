75747a0e98f0569bf21fa1d605ec1c643967f884
Andrew Howard <ahoward@galapagos.rootmypc.net>
2017-03-20 14:16:57 -0500

    Add servername options to test-ciphers

--- test-ciphers.sh
+++ test-ciphers.sh
@@ -3,10 +3,11 @@
 # Author: Unknown
 
 function usage {
-  echo "Usage: $0 [-h] [-H server-ip] [-p server-port]"
+  echo "Usage: $0 [-h] [-H server-ip] [-p server-port] [-s servername]"
   echo "  -h  Display this help"
   echo "  -H  The IP/hostname of server to test (default localhost)"
   echo "  -p  The port on the server to test (default 443)"
+  echo "  -s  Optional 'servername' argument, for SNI"
 }
 
 # OpenSSL requires the port number.
@@ -16,11 +17,13 @@ DELAY=0
 ciphers=$(openssl ciphers 'ALL:eNULL' | sed -e 's/:/ /g')
 
 # Handle command-line arguments
-while getopts ":H:p:h" ARG
+SERVERNAME=""
+while getopts ":H:p:s:h" ARG
 do
   case $ARG in
     H  ) SERVER=$OPTARG;;
     p  ) PORT=$OPTARG;;
+    s  ) SERVERNAME=$OPTARG;;
     h  ) usage && exit 1;;
     *  ) echo "Unknown argument: $OPTARG"
          usage && exit 1;;
@@ -33,7 +36,11 @@ echo "Testing ciphers supported by server at $SERVER:$PORT"
 for cipher in ${ciphers[@]}
 do
 echo -n Testing $cipher...
-result=$(echo -n | openssl s_client -cipher "$cipher" -connect $SERVER:$PORT 2>&1)
+if [[ -n "${SERVERNAME}" ]]; then
+  result=$( echo -n | openssl s_client -cipher "$cipher" -connect $SERVER:$PORT -servername "${SERVERNAME}" 2>&1 )
+else
+  result=$(echo -n | openssl s_client -cipher "$cipher" -connect $SERVER:$PORT 2>&1)
+fi
 if [[ "$result" =~ "$cipher" ]] ; then
   echo YES
 else
