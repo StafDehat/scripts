ffff8deb5f833dc51307b6acd4f93f12ccc8c871
Andrew Howard <ahoward@galapagos.rootmypc.net>
2017-03-27 16:41:21 -0500

    Replace mysqldump splitters with one-to-rule-them-all

---
+++ mysqldump-split.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+ 
+# Author: Andrew Howard
+
+SRC="${1}"
+
+mkdir ${SRC}.parts
+numParts=$(
+  awk '/-- (Current Database:|Dumping [^\s]+ for|Table structure)/{n++}
+    {print >"'${SRC}'.parts/out"n".txt"}
+    END {print n}' ${SRC}
+)
+
+cd $1.parts
+mv out.txt header.sql
+
+# Pre-process the file containing the footer.
+sed -n '/SET TIME_ZONE=@OLD_TIME_ZONE/,//p' "out${numParts}.txt" > footer.sql
+head -n -$( wc -l <footer.sql ) "out${numParts}.txt" > tmp
+mv tmp "out${numParts}.txt"
+
+DB="."
+for x in $(seq 1 ${numParts} ); do
+  COMMENT=$( head -n 1 "out${x}.txt" )
+  if grep -q '^-- Current Database:' <<<"${COMMENT}"; then
+    DB=$( cut -d'`' -f2 <<<"${COMMENT}" )
+    mkdir "${DB}"
+    mv "out${x}.txt" "${DB}.sql"
+  elif grep -q '^-- Table structure' <<<"${COMMENT}"; then
+    TBL=$( cut -d'`' -f2 <<<"${COMMENT}" )
+    mv "out${x}.txt" "${DB}"/"${TBL}.schema.sql"
+  elif grep -q '^-- Dumping data for table' <<<"${COMMENT}"; then
+    TBL=$( cut -d'`' -f2 <<<"${COMMENT}" )
+    mv "out${x}.txt" "${DB}"/"${TBL}.data.sql"
+  elif grep -q '^-- Dumping events' <<<"${COMMENT}"; then
+    mv "out${x}.txt" "${DB}"/events.sql
+  elif grep -q '^-- Dumping routines' <<<"${COMMENT}"; then
+    mv "out${x}.txt" "${DB}"/routines.sql
+  else
+    mv "out${x}.txt" "${DB}"/
+  fi
+done
+
