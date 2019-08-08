3e37c16e2d8c89b6476d88f2954f8c68d93d8bde
Andrew Howard <andrew.howard@rackspace.com>
2019-08-08 16:15:17 -0500

    Bugfix.  Input file analysis, trigger/view DB-level commands

--- mysqldump-split.sh
+++ mysqldump-split.sh
@@ -2,6 +2,10 @@
  
 # Author: Andrew Howard
 
+function debug() {
+  echo "${@}" >&2
+}
+
 function usage() {
   cat <<EOF
 Usage:
@@ -16,6 +20,8 @@ Examples:
 Supported file formats:
   ASCII text
   ASCII text (gzip compressed data)
+  Unicode text
+  Unicode text (gzip compressed data)
   POSIX tar archive (GNU) containing ASCII text
   POSIX tar archive (GNU) containing ASCII text (gzip compressed data)
 
@@ -48,6 +54,25 @@ Description:
 EOF
 }
 
+function isText() {
+  grep -qP '(ASCII|Unicode) text' <<<"${@}"
+  return $?
+}
+function isZip() {
+  grep -q 'gzip\|compressed' <<<"${@}"
+  return $?
+}
+function isTar() {
+  grep -q 'tar archive' <<<"${@}"
+  return $?
+}
+function errUnknownInput() {
+  echo "Error: Unable to identify how to extract this file." >&2
+  echo "Please give me ASCII - optionally gzip'd, tar'd, or tar'd then gzip'd." >&2
+  echo "Note: We can't handle gzip-then-tar.  Also, that's weird."
+}
+
+
 SRC="${1}"
 
 if [[ "${SRC}" == "-h" ]] ||
@@ -55,25 +80,37 @@ if [[ "${SRC}" == "-h" ]] ||
   usage && exit 0
 fi
 
-if grep -q 'gzip\|compressed' <(file "${SRC}"); then
-  catcmd='zcat'
-  if grep -q 'tar archive' <(file -z "${SRC}"); then
+#
+# Try to identify the input file format:
+if isZip $(file "${SRC}"); then
+  # Check what's inside the compression
+  if isTar $(file -z "${SRC}"); then
     catcmd='tar --to-stdout -xzf'
+  elif isText $(file -z "${SRC}"); then
+    catcmd='zcat'
   else
-    echo "Error: Unable to identify how to extract this file." >&2
-    echo "Please give me ASCII - optionally gzip'd, tar'd, or tar-gz'd." >&2
-    exit 1
+    errUnknownInput; exit 1
   fi
-elif grep -qP 'ASCII' <(file "${SRC}"); then
-  catcmd='cat'
-elif grep -q 'tar archive' <(file -z "${SRC}"); then
-  catcmd='tar --to-stdout -xf'
 else
-  echo "Error: Unable to identify how to extract this file." >&2
-  echo "Please give me ASCII - optionally gzip'd, tar'd, or tar-gz'd." >&2
-  exit 1
+  # Well it's not compressed
+  if isText $(file "${SRC}"); then
+    catcmd='cat'
+  elif isTar $(file "${SRC}"); then
+    debug "WARNING: Input format is TAR, but we can't test past that."
+    debug " Continuing on the *assumption* that it's a tar'd text file."
+    catcmd='tar --to-stdout -xf'
+  else
+    errUnknownInput; exit 1
+  fi
 fi
 
+if [[ -d "${SRC}.parts" ]]; then
+  cat <<EOF
+ERROR: Directory "${SRC}.parts" already exists.
+Aborting script with no changes, to avoid clobbering something important.
+EOF
+  exit 1
+fi
 mkdir ${SRC}.parts
 numParts=$(
   ${catcmd} "${SRC}" |
@@ -82,7 +119,17 @@ numParts=$(
     END {print n}'
 )
 
-cd $1.parts
+cd "${1}.parts"
+if ! isText $(file out.txt); then
+  mv out.txt awk-result
+  cat <<EOF
+Whatever just came out of $(basename "${SRC}"), it wasn't text.
+Instead, we found:
+  $(file awk-result)
+As such, script aborts here.
+EOF
+  exit 1
+fi
 mv out.txt 000-header.sql
 
 # Pre-process the file containing the footer.
@@ -95,8 +142,16 @@ for x in $(seq 1 ${numParts} ); do
   COMMENT=$( head -n 1 "out${x}.txt" )
   if grep -q '^-- Current Database:' <<<"${COMMENT}"; then
     DB=$( cut -d'`' -f2 <<<"${COMMENT}" )
-    mkdir "${DB}"
-    mv "out${x}.txt" "${DB}.sql"
+    if [[ ! -d "${DB}" ]]; then
+      # First occurrence - we must be creating the DB.
+      mkdir "${DB}"
+      mv "out${x}.txt" "${DB}.create.sql"
+    else
+      # Subsequent occurrence - must be triggers/views/etc that require tables to have
+      #   already been created.
+      cat "out${x}.txt" >> "${DB}.post-tables.sql"
+      rm -f "out${x}.txt"
+    fi
   elif grep -q '^-- Table structure' <<<"${COMMENT}"; then
     TBL=$( cut -d'`' -f2 <<<"${COMMENT}" )
     mv "out${x}.txt" "${DB}"/"${TBL}.schema.sql"
